d54095fd0fadd625f3d135c526f3d63508c2b7f1
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-08-01 13:53:05 +0300

    Update black.js

--- black.js
+++ black.js
@@ -46,8 +46,7 @@ client.on("ready", () => {
   console.log("بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ");
     console.log(client.guilds.cache.map(c => `${c.name} : ${c.me.hasPermission(8)} : ${c.memberCount}`));
   client.user.setActivity(`${PREFIX}help`);
-   
-  console.log(`PLAYING`);
+  console.log(`Logined`);
 });
 
 
